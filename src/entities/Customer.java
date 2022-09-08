25ddeb1261c6fceac1792c9a95b6ac1b2b5b328f
AndersonFlorencioConforto <afcaconforto@gmail.com>
2022-09-08 14:28:37 -0300

    first commit

---
+++ src/entities/Customer.java
@@ -0,0 +1,45 @@
+package entities;
+
+
+//entidade é algo que é único, ou seja cada entidade tem seu id
+// carrega valores que alteram com o tempo
+//comportamento
+//regras de negócio ou seja validaçoes etc, isso é o DDD
+//essa classe customer é uma classe anêmica, apenas carrega dados.
+public class Customer {
+
+    private Long id;
+    private String name;
+    private String address;
+
+
+    public Customer(Long id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
